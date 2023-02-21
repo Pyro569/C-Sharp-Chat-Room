6b07903e593308b1d5b86b787170c493339f1fad
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-02-20 23:07:03 -0500

    Add files via upload

---
+++ src/Server.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("");
+IPAddress ipAddress = ipHostInfo.AddressList[0];
+IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+using Socket listener = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen(100);
+
+var handler = await listener.AcceptAsync();
+
+while (true)
+{
+    // Receive message.
+    var buffer = new byte[1_024];
+    var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+    Console.WriteLine(response);
+}
